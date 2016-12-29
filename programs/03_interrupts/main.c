8de85a53272b4c09b2c74a26daa16cac560372db
yuri.panchul <yuri@panchul.com>
2016-12-28 22:22:52 -0800

    Minor

--- programs/03_interrupts/main.c
+++ programs/03_interrupts/main.c
@@ -1,8 +1,5 @@
 #include "mfp_memory_mapped_registers.h"
 
-volatile int handledsw0 = 0;
-volatile int handledsw1 = 0;
-
 volatile int toggle = 0;
 
 void __attribute__ ((interrupt)) _mips_interrupt ()
