1426ead50f9d19ca0f39f374e1e9c23620d5f780
yuri.panchul <yuri@panchul.com>
2017-01-15 22:46:22 -0800

    Developing interrupt lab

--- documentation/html_labs/lab_yp3_interrupts/r.sh
+++ documentation/html_labs/lab_yp3_interrupts/r.sh
@@ -6,7 +6,8 @@ lab_yp3_1.html \
 lab_yp3_2.html \
 lab_yp3_3.html \
 lab_yp3_4.html \
+lab_yp3_5.html \
 lab_yp3_z.html \
 > lab_yp3.html
 
-aspell -l < lab_yp3.html | sort -u > zzz_misspelled
+aspell list < lab_yp3.html | sort -u > zzz_misspelled
