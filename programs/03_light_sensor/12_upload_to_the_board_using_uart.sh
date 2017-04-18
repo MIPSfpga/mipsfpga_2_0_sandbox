93eb3734459f90ee3a7a0601c4b56009c9688767
yuri.panchul <yuri@panchul.com>
2017-04-18 11:13:45 -0700

    Added new boot

--- programs/03_light_sensor/12_upload_to_the_board_using_uart.sh
+++ programs/03_light_sensor/12_upload_to_the_board_using_uart.sh
@@ -1,2 +1,4 @@
-stty -F /dev/ttyUSB0 raw speed 115200 -crtscts cs8 -parenb -cstopb
-cat program.rec > /dev/ttyUSB0
+stty -F /dev/ttyUSB1 raw speed 115200 -crtscts cs8 -parenb -cstopb
+cat program.rec > /dev/ttyUSB1
+# stty -F /dev/ttyUSB0 raw speed 115200 -crtscts cs8 -parenb -cstopb
+# cat program.rec > /dev/ttyUSB0
--- programs/07_new_boot/mfp_memory_mapped_registers.h
+++ programs/07_new_boot/mfp_memory_mapped_registers.h
@@ -6,12 +6,14 @@
 #define MFP_SWITCHES_ADDR       0xBF800008
 #define MFP_BUTTONS_ADDR        0xBF80000C
 #define MFP_7_SEGMENT_HEX_ADDR  0xBF800010
+#define MFP_LIGHT_SENSOR_ADDR   0xBF800014
 
 #define MFP_RED_LEDS            (* (volatile unsigned *) MFP_RED_LEDS_ADDR      )
 #define MFP_GREEN_LEDS          (* (volatile unsigned *) MFP_GREEN_LEDS_ADDR    )
 #define MFP_SWITCHES            (* (volatile unsigned *) MFP_SWITCHES_ADDR      )
 #define MFP_BUTTONS             (* (volatile unsigned *) MFP_BUTTONS_ADDR       )
 #define MFP_7_SEGMENT_HEX       (* (volatile unsigned *) MFP_7_SEGMENT_HEX_ADDR )
+#define MFP_LIGHT_SENSOR        (* (volatile unsigned *) MFP_LIGHT_SENSOR_ADDR  )
 
 // This define is used in boot.S code
 
